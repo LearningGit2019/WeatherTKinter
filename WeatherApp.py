cbcd5b37aa7e444706d6c3e096ac2ba328ed5ba9
LearningGit2019@mail.com <>
2019-10-14 16:44:09 -0400

    Added button clickability, will next add weather API code

--- WeatherApp.py
+++ WeatherApp.py
@@ -5,10 +5,17 @@ import tkinter as tk
 HEIGHT = 500
 WIDTH = 600
 
+def test_function(entry):
+   print("This is the entry:", entry)
+
 root = tk.Tk()
 
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
+
+background_image = tk.PhotoImage(file="landscape.png")
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
 
 frame = tk.Frame(root, bg = "#80c1ff", bd=5)
 frame.place(relx = 0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor="n")
@@ -16,7 +23,7 @@ frame.place(relx = 0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor="n")
 entry = tk.Entry(frame, font = 40)
 entry.place(relwidth=0.65, relheight=1)
 
-button = tk.Button(frame, text="Test button", font=40)
+button = tk.Button(frame, text="Get Weather", font=40, command=lambda:test_function(entry.get()))
 button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 lower_frame = tk.Frame(root, bg="#80c1ff", bd=10)
