45202d58dde2f57da20bfd15bca0ec5fca566a37
LearningGit2019@mail.com <>
2019-10-14 16:09:39 -0400

    Adding other tkinter features

--- WeatherApp.py
+++ WeatherApp.py
@@ -10,10 +10,16 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-frame = tk.Frame(root, bg = "blue")
+frame = tk.Frame(root, bg = "#80c1ff")
 frame.place(relx = 0.1, rely=0.1, relwidth=0.8, relheight=0.8)
 
-button = tk.Button(root, text="Test button", bg="gray", fg="red" )
+button = tk.Button(frame, text="Test button", bg="gray", fg="red" )
 button.pack()
+
+label = tk.Label(frame, text="This is a label", bg="yellow")
+label.pack()
+
+entry = tk.Entry(frame, bg="green")
+entry.pack()
 
 root.mainloop()
