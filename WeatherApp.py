28910fce95130e9d73a9fc6815fddac0cd39fb25
LearningGit2019@mail.com <>
2019-10-14 16:04:52 -0400

    Updating tkinter project

--- WeatherApp.py
+++ WeatherApp.py
@@ -2,7 +2,15 @@
 
 import tkinter as tk
 
+HEIGHT = 700
+WIDTH = 800
+
 root = tk.Tk()
 
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+button = tk.Button(root, text="Test button", bg="gray", fg="red" )
+button.pack()
 
 root.mainloop()
