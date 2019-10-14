4810717ba9b02035043d236e474e7eea622f3741
LearningGit2019@mail.com <>
2019-10-14 16:07:00 -0400

    Adding a frame

--- WeatherApp.py
+++ WeatherApp.py
@@ -10,6 +10,9 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
+frame = tk.Frame(root, bg = "blue")
+frame.place(relx = 0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+
 button = tk.Button(root, text="Test button", bg="gray", fg="red" )
 button.pack()
 
